e699c178a6107f8a8496a1ef494c773503895d77
FinMartinez <finmtz@gmail.com>
2022-11-29 21:16:17 -0700

    Added import file
    DB4_B.zip
    
    doesn't seem to work...will figure it out.

--- fingerprint_scanner.py
+++ fingerprint_scanner.py
@@ -1,10 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+from zipfile import ZipFile
 
 ########################################
 #            Import files              #
 ########################################
 
+file_name = 'DB4_B.zip'
 
-print('hello world')
+with ZipFile(file_name, 'r') as zip:
+    zip.printdir()
+
+    print('Extracting all the files now...')
+    zip.extractall()
+    print('Done!')
