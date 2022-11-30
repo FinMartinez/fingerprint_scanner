845fe00d61f820658ae3d16b6b94ab0c4afff4d5
FinMartinez <finmtz@gmail.com>
2022-11-29 21:22:03 -0700

    Removed zip file

--- fingerprint_scanner.py
+++ fingerprint_scanner.py
@@ -7,11 +7,3 @@ from zipfile import ZipFile
 #            Import files              #
 ########################################
 
-file_name = 'DB4_B.zip'
-
-with ZipFile(file_name, 'r') as zip:
-    zip.printdir()
-
-    print('Extracting all the files now...')
-    zip.extractall()
-    print('Done!')
